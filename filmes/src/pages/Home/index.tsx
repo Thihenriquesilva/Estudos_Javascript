b099129381f0736470ff3211ceea58515a8b8e24
Thiago Henrique Da Silva <47490086809@info.local>
2020-10-06 11:28:41 -0300

    react-app

---
+++ filmes/src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+
+function Home() {
+  return (
+    <div>
+        <Header/>
+        <h1>Home</h1>
+        <p>Ola sou a Home, tudo bem com você?</p>
+    </div>
+  );
+}
+
+export default Home;
