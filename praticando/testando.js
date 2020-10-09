bdb84ecde81b22f0520dd107838b884d87b1e271
Thiago <henriquesilva322@outlook.com>
2020-10-09 18:30:13 -0300

    organizando...

--- praticando/testando.js
+++ praticando/testando.js
@@ -13,3 +13,21 @@ function exibirMensagem() {
 }
 
 console.log(exibirMensagem())
+
+
+
+
+var msg = "Hello World!";
+
+console.log(msg);
+
+//Função que chama a si mesma
+
+function fatorial(n){
+    if((n == 0)||(n == 1))
+        return 1;
+    else
+        return (n*(fatorial(n -1)));
+}
+
+console.log(fatorial(5));
